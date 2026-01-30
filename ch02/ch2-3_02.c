aaa4bffef11db6c0e96a73e7576b57d63ad285f7
DongjuLee <dozoo0528@gmail.com>
2026-01-30 19:40:59 +0900

    강의

---
+++ ch02/ch2-3_02.c
@@ -0,0 +1,15 @@
+//
+// Created by Dongju Lee on 2026. 1. 30..
+//
+#include <stdio.h>
+int main() {
+
+    int a = 3;
+    float b = 3.14f;
+
+    float result = a + b;
+
+    printf("결과값: %f\n",result);
+
+    return 0;
+}
