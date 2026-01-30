a2d95541311a4054c4554d054ee55c3039259746
DongjuLee <dozoo0528@gmail.com>
2026-01-30 19:40:56 +0900

    강의

---
+++ ch02/ch2-3.c
@@ -0,0 +1,16 @@
+//
+// Created by Dongju Lee on 2026. 1. 30..
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int a=5;
+    long b =10L;
+
+    long result = a + b;
+
+    printf("a+b의 결과값: %ld\n", result);
+
+    return 0;
+}
