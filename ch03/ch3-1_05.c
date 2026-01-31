b9b57116ce38e14305f88a8b188ea41cf57a5c51
DongjuLee <dozoo0528@gmail.com>
2026-01-31 19:22:44 +0900

    임베디드 필수 비트 오퍼레이터(할당 연산자)

--- ch03/ch3-1_05.c
+++ ch03/ch3-1_05.c
@@ -1,3 +1,24 @@
 //
 // Created by Dongju Lee on 2026. 1. 31..
 //
+#include <stdio.h>
+int main(void) {
+
+    int a = 21;
+    int c;
+
+    c = a;
+    printf("c = %d\n", c);
+
+    c += a; // c = c+a
+    printf("c = %d\n", c);
+
+    c -= a; // c = c-a
+    printf("c = %d\n", c);
+
+    c <<= 2; // << 비트 연산자에서도 설명한거처럼 *4
+    printf("c = %d\n", c);
+
+    c >>= 2; // >>는 비트 연산자에서도 설명한거 처럼 /4
+    printf("c = %d\n", c);
+}
