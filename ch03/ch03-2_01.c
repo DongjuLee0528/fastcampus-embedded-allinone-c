784da6fa167938a9792cc455c0321be9d9d05c93
DongjuLee <dozoo0528@gmail.com>
2026-02-01 14:52:23 +0900

    의사 결정문 이해하기

--- ch03/ch03-2_01.c
+++ ch03/ch03-2_01.c
@@ -5,7 +5,7 @@
 int main(void) {
     int a = 10;
 
-    if (a < 20 ) {
+    if (a < 20 ) { // a보다 20이 크니 참이니 밑에 있는 printf(" ais less than 20 \n"); 실행이 된다
         printf(" ais less than 20 \n");
     }
     printf("value a =%d\n", a);
