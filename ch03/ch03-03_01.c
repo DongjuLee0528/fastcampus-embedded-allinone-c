7a7cc11771d5868bde644472802c96e8fc44d269
DongjuLee <dozoo0528@gmail.com>
2026-02-02 04:05:12 +0900

    반복문의 필요성

---
+++ ch03/ch03-03_01.c
@@ -0,0 +1,14 @@
+//
+// Created by Dongju Lee on 2026. 2. 2..
+//
+#include <stdio.h>
+int main(void) {
+    int a = 10;
+
+    while (a < 20) {
+        printf("a is %d\n", a);
+        a++;
+    }
+
+    return 0;
+}
