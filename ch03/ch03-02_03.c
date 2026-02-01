60457e08d32c6f59aca9a2f3f582220153b2cf31
DongjuLee <dozoo0528@gmail.com>
2026-02-01 15:00:53 +0900

    의사 결정문 이해하기

---
+++ ch03/ch03-02_03.c
@@ -0,0 +1,15 @@
+//
+// Created by Dongju Lee on 2026. 2. 1..
+//
+#include <stdio.h>
+int main (void) {
+    int a = 100;
+
+    if (a < 20) {
+        printf("a is less than 20 \n");
+    }
+    else {
+        printf("a is not less than 20 \n");
+    }
+    printf("value a = %d\n", a);
+}
