7b8104256b386ed9d01e20cc2be4aa7cd5978a46
DongjuLee <dozoo0528@gmail.com>
2026-02-04 20:08:12 +0900

    그림으로 배우는 포인터 (이 함수에서 a b값은 왜 그대로 일까?)

--- ch03/ch03-06_01.c
+++ ch03/ch03-06_01.c
@@ -16,7 +16,7 @@ void swap(int x, int y) {
 
 int main(void) {
 
-    int a = 100;
+    int a = 100;// 함수에 전달 되는 값은 이 int a의 값을 복사해서 준다 그러므로 a b의 값은 변경이 안된다 변경하고 싶으면 메모리 주소를 줘야한다
     int b = 200;
 
     printf("Before swap(), a = %d\n", a);
