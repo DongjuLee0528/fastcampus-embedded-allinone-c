ce6638e4f98695bce66280f6c9526949a76ff197
DongjuLee <dozoo0528@gmail.com>
2026-02-10 21:33:50 +0900

    메모리 최적화 Union 개념과 예제3

---
+++ ch05/ch05-02_04.c
@@ -0,0 +1,25 @@
+//
+// Created by Dongju Lee on 2026. 2. 10..
+//
+#include <stdio.h>
+#include <string.h>
+
+union Data {
+    int i;
+    float f;
+    char str[20];
+};
+int main (void) {
+    union Data data;
+
+    data.i = 10;
+    printf("data.i = %d\n", data.i);
+
+
+    data.f = 220.5;
+    printf("data.f = %f\n", data.f);
+
+    strcpy(data.str, "Dong ju LEE");
+    printf("data.str = %s\n", data.str);
+
+}
