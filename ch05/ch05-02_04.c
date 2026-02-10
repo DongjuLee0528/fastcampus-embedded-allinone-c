0a5c3c2fd14f624783a0521f526d6d61e4600393
DongjuLee <dozoo0528@gmail.com>
2026-02-10 21:39:41 +0900

    메모리 최적화 Union 개념과 예제3

--- ch05/ch05-02_04.c
+++ ch05/ch05-02_04.c
@@ -1,6 +1,7 @@
 //
 // Created by Dongju Lee on 2026. 2. 10..
 //
+// 올바른 사용법
 #include <stdio.h>
 #include <string.h>
 
@@ -11,7 +12,7 @@ union Data {
 };
 int main (void) {
     union Data data;
-
+    // 값을 할당한 멤버는 다른 멤버에 할당하기전에 사용해버린다
     data.i = 10;
     printf("data.i = %d\n", data.i);
 
