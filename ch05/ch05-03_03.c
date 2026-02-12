3548fcb839e769e715e8ddd3e9a6d2f5f075ca72
DongjuLee <dozoo0528@gmail.com>
2026-02-12 10:58:24 +0900

    메모리 최적화 bit fields 개념과 예제 비트 필드 예제 2 주석

--- ch05/ch05-03_03.c
+++ ch05/ch05-03_03.c
@@ -3,19 +3,21 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
-
+// 비트 필드 사용할때 주의 할 부분은 값의 범위
 struct {
-    unsigned int age : 3;
+    unsigned int age : 3; //3비트만 사용하겠음
+    //3비트는 0 ~ 7까지 표현 가능
 }Age;
 int main(void) {
-    Age.age = 4;
+    Age.age = 4;// 4라는 값 사용가능
     printf("sizeof(Age)=%d\n", sizeof(Age));
     printf("Age.age=%d\n", Age.age);
 
-    Age.age = 7;
+    Age.age = 7; // 7도 사용가능
     printf("Age.age=%d\n", Age.age);
 
-    Age.age = 8;
+    Age.age = 8; // 8은 4비트 필요하여 0이 나오는 오버플로우 나옴
+    // 내가 저장하는 값을 계산해 비트를 할당을 해줌
     printf("Age.age=%d\n", Age.age);
 
     return 0;
