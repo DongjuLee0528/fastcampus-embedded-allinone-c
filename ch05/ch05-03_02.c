b3ba74f1414c39646ace027fc59254ad10fdf0ac
DongjuLee <dozoo0528@gmail.com>
2026-02-12 10:49:53 +0900

    메모리 최적화 bit fields 개념과 예제 비트 필드 예제 1 주석

--- ch05/ch05-03_02.c
+++ ch05/ch05-03_02.c
@@ -6,10 +6,13 @@
 struct {
     unsigned int a;
     unsigned int b;
+    // 총 8바이트 사용
 }status1;
 struct {
-    unsigned int a : 1;
+    // 같은 타입 변수 안에서 비트를 나눠 쓰는 기능이다
+    unsigned int a : 1; //1 bit 만 사용한다라는 것과 같다
     unsigned int b : 1;
+    // 총 4 바이트 사용
 }status2;
 
 int main(void) {
