c8f847903ef2859c5d903567f26d6d8210e1ff87
DongjuLee <dozoo0528@gmail.com>
2026-02-12 10:06:16 +0900

    메모리 최적화 bit fields 개념과 예제 비트 필드 선언

---
+++ ch05/ch05-03_01.c
@@ -0,0 +1,19 @@
+//
+// Created by Dongju Lee on 2026. 2. 12..
+//
+//bit fields를 사용하면 메모리나 데이터 저장 공간을 효율적으로 사용아 가능하다.
+// 임베디드 시스템 같이 메모리가 부족한 상황에서는 필수적으로 사용되는 기술이다.
+#include <stdio.h>
+/*
+struct {
+    member definition : width;
+    member definition : width;
+};[struct name];
+
+ex)
+union Data {
+    int i;
+    float f;
+    char str [20];
+} data
+*/
